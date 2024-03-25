abd85f8e19f168a688c99524c033e0e81b21172b
Nikonoff <nikchumadev@gmail.com>
2024-03-25 20:14:11 +0700

    Get, Post, Delete done

--- libhv-http/src/Routers.cpp
+++ libhv-http/src/Routers.cpp
@@ -1,46 +1,9 @@
 #include "Routers.hpp"
-#include <map>
 
-std::map<int, nlohmann::json> users;
+nlohmann::json users;
 
 void route::RegisterResources(hv::HttpService &router)
 {
-    // router.GET("/hello", [](HttpRequest *req, HttpResponse *resp)
-    // {
-    //     nlohmann::json response
-    //     {
-    //         {"куе", "Hello world"}
-    //     };
-
-    //     resp->SetBody(response.dump());
-    //     resp->content_type = APPLICATION_JSON;
-
-    //     return 200;
-    // });
-
-    // router.POST("/hello", [](HttpRequest *req, HttpResponse *resp)
-    // {
-    //     nlohmann::json request;
-    //     nlohmann::json response;
-
-    //     try
-    //     {
-    //         request = nlohmann::json::parse(req->body);
-    //     }
-    //     catch(const std::exception& e)
-    //     {
-    //         response["error"] = "Invalid JSON";
-    //         resp->SetBody(response.dump());
-    //         resp->content_type = APPLICATION_JSON;
-    //         return 400;
-    //     }
-        
-    //     resp->SetBody(request.dump());
-    //     resp->content_type = APPLICATION_JSON;
-
-    //     return 200;
-    // });
-
     router.POST("/user", [](HttpRequest *req, HttpResponse *resp)
     {
         nlohmann::json request;
@@ -49,9 +12,14 @@ void route::RegisterResources(hv::HttpService &router)
         try
         {
             request = nlohmann::json::parse(req->body);
-            int id = users.size();
-            users[id] = request; 
-            response["id"] = id;
+
+            for (auto &user : users)
+            {
+                if (request["id"].get<int>() == user["id"].get<int>())
+                    throw std::exception();
+            }
+
+            users.push_back(request); 
             response["msg"] = "User added successfully";
         }
         catch(const std::exception& e)
@@ -71,20 +39,36 @@ void route::RegisterResources(hv::HttpService &router)
     router.GET("/user/{userId}", [](HttpRequest *req, HttpResponse *resp)
     {
         nlohmann::json response;
-        int userId = std::stoi(req->GetParam("userId"));
 
-        if (users.find(userId) != users.end())
+        try
         {
-            response = users[userId];
+            int userId = std::stoi(req->GetParam("userId"));
+            
+            for (auto &user : users)
+            {
+                if (user["id"].get<int>() == userId){
+                    response["data"] = user;
+                    break;
+                }
+            }
+            
+            if (response.empty())
+            {
+                response["error"] = "User not found";
+                resp->SetBody(response.dump());
+                resp->content_type = APPLICATION_JSON;
+                return 404;
+            }
+
         }
-        else
+        catch(const std::exception& e)
         {
-            response["error"] = "User not found";
+            response["error"] = "Invalid data";
             resp->SetBody(response.dump());
             resp->content_type = APPLICATION_JSON;
-            return 404;
+            return 400;
         }
-
+        
         resp->SetBody(response.dump());
         resp->content_type = APPLICATION_JSON;
 
@@ -93,14 +77,19 @@ void route::RegisterResources(hv::HttpService &router)
 
     router.GET("/users", [](HttpRequest *req, HttpResponse *resp)
     {
-        nlohmann::json response = nlohmann::json::array();
-
-        for (auto &user : users)
+        nlohmann::json response;
+        try{
+            if (!users.empty())
+                response["data"] = users;
+            else
+                response["msg"] = "There are no users";
+        }
+        catch(const std::exception& e)
         {
-            nlohmann::json userJson;
-            userJson["id"] = user.first;
-            userJson["data"] = user.second;
-            response.push_back(userJson);
+            response["error"] = "Invalid data";
+            resp->SetBody(response.dump());
+            resp->content_type = APPLICATION_JSON;
+            return 400;
         }
 
         resp->SetBody(response.dump());
@@ -112,19 +101,34 @@ void route::RegisterResources(hv::HttpService &router)
     router.Delete("/user/{userId}", [](HttpRequest *req, HttpResponse *resp)
     {
         nlohmann::json response;
-        int userId = std::stoi(req->GetParam("userId"));
 
-        if (users.find(userId) != users.end())
+        try
         {
-            users.erase(userId);
-            response["msg"] = "User deleted successfully";
+            int userId = std::stoi(req->GetParam("userId"));
+            
+            auto it = users.begin();
+            for(; it != users.end(); ++it) {
+                if(it->at("id") == userId)
+                    break;
+            }
+
+            if(it != users.end()){
+                users.erase(it);
+                response["msg"] = "User deleted successfully";
+            }
+            else{
+                response["error"] = "User not found";
+                resp->SetBody(response.dump());
+                resp->content_type = APPLICATION_JSON;
+                return 404;
+            }
         }
-        else
+        catch(const std::exception& e)
         {
-            response["error"] = "User not found";
+            response["error"] = "Invalid data";
             resp->SetBody(response.dump());
             resp->content_type = APPLICATION_JSON;
-            return 404;
+            return 400;
         }
 
         resp->SetBody(response.dump());
